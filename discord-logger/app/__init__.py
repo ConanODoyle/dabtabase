3bf098feee0c3ecd0aff01ec86adafd1e9a39c46
ConanODoyle <samuel.w.sze@gmail.com>
2020-08-24 14:45:03 -0700

    Implement user login and registration

--- discord-logger/app/__init__.py
+++ discord-logger/app/__init__.py
@@ -2,10 +2,13 @@ from flask import Flask
 from config import DefaultConfig
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_login import LoginManager
 
 flaskapp = Flask(__name__)
 flaskapp.config.from_object(DefaultConfig)
 db = SQLAlchemy(flaskapp)
 migrate = Migrate(flaskapp, db)
+login = LoginManager(flaskapp)
+login.login_view = 'login'
 
 from app import routes, models
--- discord-logger/app/routes.py
+++ discord-logger/app/routes.py
@@ -1,10 +1,14 @@
-from flask import render_template, flash, redirect, url_for
-from app import flaskapp
-from app.forms import LoginForm
+from flask import render_template, flash, redirect, url_for, request
+from flask_login import current_user, login_user, logout_user, login_required
+from werkzeug.urls import url_parse
+from app import flaskapp, db
+from app.forms import LoginForm, RegistrationForm
+from app.models import User
 
 
 @flaskapp.route("/")
 @flaskapp.route("/index")
+@login_required
 def index():
 	user = {"username": "Miguel"}
 	posts = [
@@ -17,14 +21,49 @@ def index():
 			"body": "e"
 		}
 	]
-	return render_template("index.html", **{"user": user, "title": "test", "posts": posts})
+	return render_template("index.html", **{"title": "test", "posts": posts})
 
 
 @flaskapp.route("/login", methods=["GET", "POST"])
 def login():
-	loginform = LoginForm()
-	if loginform.validate_on_submit():
-		flash("Login requested for user {}, remember_me={}".format(loginform.username.data, loginform.remember_me.data))
+	if current_user.is_authenticated:
 		return redirect(url_for('index'))
 
+	loginform = LoginForm()
+	if loginform.validate_on_submit():
+		user = User.query.filter_by(username=loginform.username.data).first()
+		if user is None or not user.check_password(loginform.password.data):
+			flash('Invalid username or password')
+			return redirect(url_for('login'))
+		login_user(user, remember=loginform.remember_me.data)
+
+		next_page = request.args.get('next')
+		if not next_page or url_parse(next_page).netloc != '':
+			next_page = url_for('index')
+
+		return redirect(next_page)
+
 	return render_template("login.html", title="Sign In", form=loginform)
+
+
+@flaskapp.route('/logout')
+def logout():
+	logout_user()
+	return redirect(url_for('index'))
+
+
+@flaskapp.route('/register', methods=['GET', 'POST'])
+def register():
+	if current_user.is_authenticated:
+		return redirect(url_for('index'))
+
+	registerform = RegistrationForm()
+	if registerform.validate_on_submit():
+		user = User(username=registerform.username.data, email=registerform.email.data)
+		user.set_password(registerform.password.data)
+		db.session.add(user)
+		db.session.commit()
+		flash('Registration successful!')
+		return redirect(url_for('login'))
+
+	return render_template('register.html', title='Register', form=registerform)
